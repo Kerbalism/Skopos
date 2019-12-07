34339d331e00338ef81dd3c5a43171c1622e6ae1
SirMortimer <his.mortimerness@gmail.com>
2019-12-07 23:36:05 +0100

    Added a class to help interact with Kerbalism, some documentation and a sample use case

--- src/KerbalismContracts/Modules/KerbalismContractEquipment.cs
+++ src/KerbalismContracts/Modules/KerbalismContractEquipment.cs
@@ -62,6 +62,15 @@ namespace Kerbalism.Contracts
 			}
 		}
 
+		/// <summary>
+		/// This will be called by Kerbalism. The Method must have exactly this name, must be static and must accept these parameters.
+		/// </summary>
+		/// <param name="vessel">Vessel (unloaded)</param>
+		/// <param name="proto_part">part snapshot, this contains the persisted state of the part you're dealing with</param>
+		/// <param name="proto_module">module snapshot, this contains the persisted state of the module you're dealing with</param>
+		/// <param name="pm">proto module, not an actual instance you can work with. values from configuration will be set, persisted values will not be set.</param>
+		/// <param name="p">proto part, in case you need it</param>
+		/// <param name="elapsed_s">time elapsed since the last call. this can be very long depending on the current warp speed</param>
 		public static void BackgroundUpdate(Vessel vessel, ProtoPartSnapshot proto_part, ProtoPartModuleSnapshot proto_module, PartModule pm, Part p, double elapsed_s)
 		{
 			KerbalismContractEquipment module = pm as KerbalismContractEquipment;
@@ -73,6 +82,7 @@ namespace Kerbalism.Contracts
 					.Consume(module.resourceName, module.resourceRate)
 					.Execute(vessel, module.title, elapsed_s);
 
+				// rate will be < 1 if the process did not have enough input resources to run for the entire duration
 				EquipmentStateTracker.Update(vessel, module.id, rate > 0.99);
 			}
 		}
--- src/KerbalismContracts/Modules/KerbalismUtils.cs
+++ src/KerbalismContracts/Modules/KerbalismUtils.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Reflection;
 
 namespace Kerbalism.Contracts
 {
+	/// <summary>
+	/// Wrapper class that either handles resources using the stock KSP resource handler,
+	/// or uses the Kerbalsim resource system. Detects if Kerbalism is loaded and will
+	/// call the Kerbalism API if it's there.
+	///
+	/// For the stock resource consumption to work, your part module must implement IResourceConsumer
+	/// (return GetConsumedResources from this class). It also must call this FixedUpdate from its own FixedUpdate.
+	/// </summary>
 	public class KerbalismResourceHandler
 	{
 		public PartModule partModule { get; private set; }
 		public List<PartResourceDefinition> consumedResources { get; private set; }
 		public List<ModuleResource> inputResources { get; private set; }
 
-		private bool KerbalismPresent = true;
 		private string title;
 		private double lastFixedUpdate;
 
-		internal KerbalismResourceHandler(PartModule partModule, string title = null)
+		/// <summary>
+		/// Instantiate this in your part module. title will appear in a tool tip window
+		/// when you hover over the Kerbalism resource consumption displays.
+		/// </summary>
+		/// <param name="partModule"></param>
+		/// <param name="title"></param>
+		public KerbalismResourceHandler(PartModule partModule, string title = null)
 		{
 			this.partModule = partModule;
 			this.title = title;
@@ -24,14 +37,16 @@ namespace Kerbalism.Contracts
 		}
 
 		/// <summary>
+		/// Call this from your part modules OnLoad.
 		/// Add an input resource to be consumed by the part module.
+		/// Safe to call multiple times with the same values.
 		/// </summary>
 		/// <param name="resourceName"></param>
 		/// <param name="resourceRate"></param>
 		internal void AddInputResource(string resourceName, double resourceRate)
 		{
-			if (KerbalismPresent && inputResources == null) inputResources = new List<ModuleResource>();
-			List<ModuleResource> list = KerbalismPresent ? inputResources : partModule.resHandler.inputResources;
+			if (KerbalismAPI.Available && inputResources == null) inputResources = new List<ModuleResource>();
+			List<ModuleResource> list = KerbalismAPI.Available ? inputResources : partModule.resHandler.inputResources;
 
 			// KSP calls OnLoad twice, so double-check if we added the resource already before we add it a second time.
 			foreach(var resource in list)
@@ -52,14 +67,17 @@ namespace Kerbalism.Contracts
 			list.Add(moduleResource);
 		}
 
-		internal void OnAwake()
+		/// <summary>
+		/// Call this from your part modules OnAwake.
+		/// </summary>
+		public void OnAwake()
 		{
 			if (consumedResources == null)
 				consumedResources = new List<PartResourceDefinition>();
 			else
 				consumedResources.Clear();
 
-			if (KerbalismPresent)
+			if (KerbalismAPI.Available)
 				return;
 
 			if (inputResources == null) inputResources = new List<ModuleResource>();
@@ -71,9 +89,14 @@ namespace Kerbalism.Contracts
 				consumedResources.Add(PartResourceLibrary.Instance.GetDefinition(inResources[i].name));
 		}
 
-		internal double FixedUpdate(ref string status)
+		/// <summary>
+		/// Call this from your PartModules static FixedUpdate method.
+		/// </summary>
+		/// <param name="status"></param>
+		/// <returns></returns>
+		public double FixedUpdate(ref string status)
 		{
-			if(!KerbalismPresent)
+			if(!KerbalismAPI.Available)
 				return partModule.resHandler.UpdateModuleResourceInputs(ref status, 1.0, 0.99, false, false, true);
 
 			if(lastFixedUpdate == 0)
@@ -94,7 +117,11 @@ namespace Kerbalism.Contracts
 			return rate;
 		}
 
-		internal List<PartResourceDefinition> GetConsumedResources()
+		/// <summary>
+		/// Call this from your PartModules GetConsumedResources
+		/// </summary>
+		/// <returns></returns>
+		public List<PartResourceDefinition> GetConsumedResources()
 		{
 			return consumedResources;
 		}
@@ -177,22 +204,52 @@ namespace Kerbalism.Contracts
 	/// </summary>
 	public static class KerbalismAPI
 	{
+		private static Type API;
+		private static MethodInfo API_ProduceResource;
+		private static MethodInfo API_ConsumeResource;
+		private static MethodInfo API_ResourceAmount;
+		internal static readonly bool Available;
+
+		static KerbalismAPI()
+		{
+			foreach (var a in AssemblyLoader.loadedAssemblies)
+			{
+				if (a.name.StartsWith("Kerbalism1", StringComparison.Ordinal))
+				{
+					API = a.assembly.GetType("KERBALISM.API");
+					if(API != null)
+					{
+						API_ResourceAmount = API.GetMethod("ResourceAmount");
+						API_ProduceResource = API.GetMethod("ProduceResource");
+						API_ConsumeResource = API.GetMethod("ConsumeResource");
+					}
+					Available = API != null;
+				}
+			}
+		}
+
 		public static double ResourceAvailable(Vessel vessel, string resource_name)
 		{
-			return KERBALISM.API.ResourceAmount(vessel, resource_name);
+			if (API == null) return 0.0;
+			return (double)API_ResourceAmount.Invoke(null, new object[] { vessel, resource_name });
 		}
 
 		public static void ConsumeResource(Vessel vessel, string resource_name, double quantity, string title)
 		{
-			KERBALISM.API.ConsumeResource(vessel, resource_name, Math.Abs(quantity), title);
+			if (API == null) return;
+			API_ConsumeResource.Invoke(null, new object[] { vessel, resource_name, Math.Abs(quantity), title });
 		}
 
 		public static void ProduceResource(Vessel vessel, string resource_name, double quantity, string title)
 		{
-			KERBALISM.API.ProduceResource(vessel, resource_name, Math.Abs(quantity), title);
+			if (API == null) return;
+			API_ProduceResource.Invoke(null, new object[] { vessel, resource_name, Math.Abs(quantity), title });
 		}
 	}
 
+	/// <summary>
+	/// Helper function to interact with proto modules of unloaded vessels. You will need this a lot.
+	/// </summary>
 	public static class Proto
 	{
 		public static bool GetBool(ProtoPartModuleSnapshot m, string name, bool def_value = false)
@@ -202,16 +259,66 @@ namespace Kerbalism.Contracts
 			return s != null && bool.TryParse(s, out v) ? v : def_value;
 		}
 
+		public static uint GetUInt(ProtoPartModuleSnapshot m, string name, uint def_value = 0)
+		{
+			uint v;
+			string s = m.moduleValues.GetValue(name);
+			return s != null && uint.TryParse(s, out v) ? v : def_value;
+		}
+
+		public static int GetInt(ProtoPartModuleSnapshot m, string name, int def_value = 0)
+		{
+			int v;
+			string s = m.moduleValues.GetValue(name);
+			return s != null && int.TryParse(s, out v) ? v : def_value;
+		}
+
+		public static float GetFloat(ProtoPartModuleSnapshot m, string name, float def_value = 0.0f)
+		{
+			// note: we set NaN and infinity values to zero, to cover some weird inter-mod interactions
+			float v;
+			string s = m.moduleValues.GetValue(name);
+			return s != null && float.TryParse(s, out v) && !float.IsNaN(v) && !float.IsInfinity(v) ? v : def_value;
+		}
+
+		public static double GetDouble(ProtoPartModuleSnapshot m, string name, double def_value = 0.0)
+		{
+			// note: we set NaN and infinity values to zero, to cover some weird inter-mod interactions
+			double v;
+			string s = m.moduleValues.GetValue(name);
+			return s != null && double.TryParse(s, out v) && !double.IsNaN(v) && !double.IsInfinity(v) ? v : def_value;
+		}
+
 		public static string GetString(ProtoPartModuleSnapshot m, string name, string def_value = "")
 		{
 			string s = m.moduleValues.GetValue(name);
 			return s ?? def_value;
 		}
 
-		public static double GetDouble(ProtoPartModuleSnapshot m, string name, double def_value = 0.0)
+		public static T GetEnum<T>(ProtoPartModuleSnapshot m, string name, T def_value)
 		{
 			string s = m.moduleValues.GetValue(name);
-			return s == null ? def_value : double.Parse(s);
+			if (s != null && Enum.IsDefined(typeof(T), s))
+			{
+				T forprofiling = (T)Enum.Parse(typeof(T), s);
+				UnityEngine.Profiling.Profiler.EndSample();
+				return forprofiling;
+			}
+			return def_value;
+		}
+
+		public static T GetEnum<T>(ProtoPartModuleSnapshot m, string name)
+		{
+			string s = m.moduleValues.GetValue(name);
+			if (s != null && Enum.IsDefined(typeof(T), s))
+				return (T)Enum.Parse(typeof(T), s);
+			return (T)Enum.GetValues(typeof(T)).GetValue(0);
+		}
+
+		///<summary>set a value in a proto module</summary>
+		public static void Set<T>(ProtoPartModuleSnapshot module, string value_name, T value)
+		{
+			module.moduleValues.SetValue(value_name, value.ToString(), true);
 		}
 	}
 }
